da3641acbcb8a486a050ba85516b0fd63e856784
Rafael Eguren <rafaegur29@gmail.com>
2024-08-22 19:17:00 -0300

    commit inicial

---
+++ app/src/main/java/com/example/utnfrlp/moviles/tp1_moviles/Login.kt
@@ -0,0 +1,15 @@
+package com.example.utnfrlp.moviles.tp1_moviles
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+    }
+}
