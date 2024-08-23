b0f752253e9dff6c7af0aed038193322e9df58fa
Valentin Peres <peresvalentin09@gmail.com>
2024-08-22 21:38:58 -0300

    [Feat] : Agrego input para usuario

--- app/src/main/java/com/example/utnfrlp/moviles/tp1_moviles/Login.kt
+++ app/src/main/java/com/example/utnfrlp/moviles/tp1_moviles/Login.kt
@@ -1,6 +1,7 @@
 package com.example.utnfrlp.moviles.tp1_moviles
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,7 +10,8 @@ import androidx.core.view.WindowInsetsCompat
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        val usernameInput = findViewById<EditText>(R.id.username) //Creo una variable para ingresar el usuario y segun el tipo de label (EditText en este caso) y una id me lo linkea con la vista.
     }
 }
