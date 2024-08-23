87ce06d69fef526632a703af47672b0377a8d5f5
Valentin Peres <peresvalentin09@gmail.com>
2024-08-23 16:00:30 -0300

    [Fix] : Agrego linea para que no marque como error el texto hardcodeado

--- app/src/main/java/com/example/utnfrlp/moviles/tp1_moviles/WelcomeActivity.kt
+++ app/src/main/java/com/example/utnfrlp/moviles/tp1_moviles/WelcomeActivity.kt
@@ -13,7 +13,7 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-
+        //Inicializo las componentes que voy a utilizar (Encuentro su id dispuesto en el xml, etc
         val welcomeMessage = findViewById<TextView>(R.id.welcomeMessage)
         val username = intent.getStringExtra("USERNAME")
         welcomeMessage.text = "¡Bienvenido, $username!"
@@ -22,7 +22,7 @@ class WelcomeActivity : AppCompatActivity() {
         val platformLogo = findViewById<ImageView>(R.id.platformLogo)
         val otherPreferenceInput = findViewById<EditText>(R.id.otherPreferenceInput)
         val checkOther = findViewById<CheckBox>(R.id.checkOther)
-
+        //Funcion que se encarga de chequear que sistema operativo se elije, en el xml Agrupe los botones en un radio, depende cual elija, con su id determino que imagen mostrar
         platformRadioGroup.setOnCheckedChangeListener { group, checkedId ->
             val selectedPlatform = findViewById<RadioButton>(checkedId)
             when (selectedPlatform.id) {
@@ -35,7 +35,7 @@ class WelcomeActivity : AppCompatActivity() {
             }
         }
 
-
+        //Si la caja de otros esta seleccionado desplega el texto visible para que se pueda escribir
         checkOther.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 otherPreferenceInput.visibility = EditText.VISIBLE
