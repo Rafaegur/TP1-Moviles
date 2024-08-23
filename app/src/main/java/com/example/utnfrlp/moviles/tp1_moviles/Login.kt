e9d067527c2e00e05fddb3225d5a6edbee1ff96e
Valentin Peres <peresvalentin09@gmail.com>
2024-08-22 22:04:12 -0300

    [Feat] : Agrego logica de inicio y registro

--- app/src/main/java/com/example/utnfrlp/moviles/tp1_moviles/Login.kt
+++ app/src/main/java/com/example/utnfrlp/moviles/tp1_moviles/Login.kt
@@ -6,6 +6,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +17,21 @@ class Login : AppCompatActivity() {
 
         val usernameInput = findViewById<EditText>(R.id.username) //Creo una variable para ingresar el usuario y segun el tipo de label (EditText en este caso) y una id me lo linkea con la vista.
         val passwordInput = findViewById<EditText>(R.id.password) //Misma logica pero para la contraseña
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val registerTextView = findViewById<TextView>(R.id.registerTextView)
+        loginButton.setOnClickListener { //Cuando se clickea el boton disparatodo el codigo que va adentro de las llaves
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            if (username == "Juan Torres" && password == "1234utn") {
+                Toast.makeText(this, "Login exitoso", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            }
+        }
+        registerTextView.setOnClickListener {
+            //
+        }
+
     }
 }
